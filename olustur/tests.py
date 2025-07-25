b231fc539d8bb1f26ae2f77573519820260d6c5b
Esat <saygina7@gmail.com>
2025-07-26 01:38:42 +0300

    ilk test kodu eklendi

--- olustur/tests.py
+++ olustur/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class HomeTests(TestCase):
+    def test_sayfaya_aciliyor_mu(self):
+        response = self.client.get(reverse("olustur:home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "URL Adresi Girin:")
